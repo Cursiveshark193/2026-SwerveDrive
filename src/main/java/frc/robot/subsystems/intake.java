6f51bdfa0e7d3ca2748dcf25fdbfb8679fb51875
frunkletta <beardieboymcg@gmail.com>
2026-01-30 12:55:44 -0700

    modified:   src/main/java/frc/robot/subsystems/intake.java

--- src/main/java/frc/robot/subsystems/intake.java
+++ src/main/java/frc/robot/subsystems/intake.java
@@ -1,7 +1,7 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-/* 
+ 
 package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Amps;
@@ -69,7 +69,7 @@ public class intake extends SubsystemBase {
 
   // Shooter Mechanism
   private FlyWheel intake = new FlyWheel(intakeConfig);
-*/
+
   /**
    * Gets the current velocity of the shooter.
    *
@@ -121,7 +121,7 @@ public class intake extends SubsystemBase {
    // return false;
   //}
 
- /*  @Override
+   @Override
   public void periodic() {
   // This method will be called once per scheduler run
     intake.updateTelemetry();  }
@@ -131,4 +131,3 @@ public class intake extends SubsystemBase {
     intake.simIterate();
   }
 }
-*/
